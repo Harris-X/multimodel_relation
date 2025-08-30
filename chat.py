fd7947017e22822f603ef1358d382c39c5bb56ce
harrisx <1042692670@qq.com>
2025-08-30 22:20:13 +0800

    update

--- chat.py
+++ chat.py
@@ -5,26 +5,27 @@ from transformers import AutoProcessor, Glm4vForConditionalGeneration
 import torch
 from PIL import Image
 
-MODEL_PATH = "/home/user/xieqiuhao/multimodel_relation/downloaded_model/GLM-4.1V-9B-Base"
+MODEL_PATH = "/home/user/xieqiuhao/multimodel_relation/downloaded_model/GLM-4.1V-9B-Thinking"
 
 # 加载本地图片
-image_path1 = "/home/user/xieqiuhao/multimodel_relation/datasets/1-150/rgb_img/125.jpg"
+image_path1 = "/home/user/xieqiuhao/multimodel_relation/datasets/1-150/rgb_img/97.jpg"
 image_path2 = "/home/user/xieqiuhao/multimodel_relation/datasets/1-150/thermal_img/125.jpg"
 
 prompt = (
     "现给出一对图片对或者图片-文本对，识别图像目标主体的行动意图和场景，以及图像中各个主体之间的关系或者文本的正文细节内容。"
     "只依据图片场景意思和文本语义内容，忽略图像不同表现形式（色彩、风格等）以及不同成像技术（可见光、热成像等），"
-    "分析它们之间是什么关系，关系类型分别是关联、因果、等价、矛盾。可利用假设法、排除法等方式。"
-    "输出要求：首先给出关系类型判断，然后给出分析。注意，1）所有关系的判断忽略表现形式上（如RGB图像-热力图的成像方式）的不同，只关注图像或者文本的表达意思；"
+    "分析它们之间是什么关系，关系类型分别是关联、因果、等价、矛盾。。"
+    "输出要求：首先给出关系类型判断，然后给出分析。"
+    "注意，1）所有关系的判断忽略表现形式上（如RGB图像-热力图的成像方式）的不同，只关注图像或者文本的表达意思；"
     "2）等价只要求图片场景内容的意思或文本表达的意思相同即可；"
     "3）矛盾关系不关注格式、色彩、成像方式，只关注图像场景内容或者文本语义内容之间是否存在语义上的矛盾关系，比如二者同时某个主体都存在，但同一主体所在的场景或者做的事情不同，"
-    "或者因绝对化语气（修饰词、量词等）比如人数说法等导致场景、行动意图、行动方式、人员数量冲突等；"
+    "或者因绝对化语气（形容词、修饰词、量词等）比如人数量词等导致场景、行动意图、行动方式、与人员数量冲突等；"
     "4）所有的数据都假设是对敌方的记录，不包含本方相关场景、文本的记录。"
 )
 
 # 可选文本；如无则仅图片+提示词
 text = None
-# text = "密级：秘密  等级：紧急  时间：2023年5月10日  发报：军情局  收报：前线指挥部  抄送：各相关部门  主题：观察报告  正文：  敌方全部人员正在城市中休整"
+text = "密级：秘密  等级：紧急  时间：2023年5月10日  发报：军情局  收报：前线指挥部  抄送：各相关部门  主题：观察报告  正文：  敌方全部人员正在城市中休整"
 
 def build_initial_messages(image1: Image.Image, image2: Image.Image, prompt_text: str, extra_text: str | None):
     if extra_text:
@@ -34,7 +35,7 @@ def build_initial_messages(image1: Image.Image, image2: Image.Image, prompt_text
             "role": "user",
             "content": [
                 {"type": "image", "image": image1},
-                {"type": "image", "image": image2},
+                # {"type": "image", "image": image2},
                 {"type": "text", "text": prompt_text},
             ],
         }
